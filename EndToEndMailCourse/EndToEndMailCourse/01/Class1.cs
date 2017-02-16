ad27b1d09dae0cb8f2f41d67c6490b9d4a07ee86
Adam Osiecki <osiecki.adam@outlook.com>
2017-02-17 00:51:45 +0100

    Hello World Commit

---
+++ EndToEndMailCourse/EndToEndMailCourse/01/Class1.cs
@@ -0,0 +1,41 @@
+﻿
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EndToEndMailCourse._01
+{
+    [TestFixture]
+    public class FirstTestClass
+    {
+        private string testUrl = "https://suvroc.github.io/selenium-mail-course/01/example.html";
+
+        [Test]
+        public void FirstTest()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(testUrl);
+
+
+            driver.Quit();
+        }
+
+        [Test]
+        public void ShouldTestFirstName()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(testUrl);
+
+            var element = driver.FindElement(By.Name("elementWithName"));
+            Assert.IsNotNull(element);
+            Assert.AreEqual(element.GetAttribute("value"), "Name test data");
+        }
+    }
+}
+
