1c3c850a8cc2b7a39efa17be2dc17823edd72382
Adrian Sroka <adrian.sroka@britishcouncil.pl>
2017-02-21 11:59:25 +0100

    Lesson 2 files

---
+++ EndToEndMailCourse/EndToEndMailCourse/02/Workshop02Tests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace EndToEndMailCourse._02
+{
+    [TestFixture]
+    public class Workshop02Tests
+    {
+        private string testUrl = "https://suvroc.github.io/selenium-mail-course/02/workshop.html";
+
+        [Test]
+        public void ShouldTestWorkshop2Page()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(testUrl);
+            string name = "Test name";
+            string comment = "Test comment";
+            IWebElement nameResult = null,
+                commentResult = null;
+
+            #region TEST CODE
+
+            #endregion
+
+            nameResult = driver.FindElement(By.Id("savedTaskName"));
+            commentResult = driver.FindElement(By.Id("savedComment"));
+
+            Assert.NotNull(nameResult);
+            Assert.AreEqual(nameResult.TagName, "div");
+            Assert.AreEqual(nameResult.Text, name);
+            Assert.IsTrue(nameResult.Displayed);
+
+            Assert.NotNull(commentResult);
+            Assert.AreEqual(commentResult.TagName, "div");
+            Assert.AreEqual(commentResult.Text, comment);
+            Assert.IsTrue(nameResult.Displayed);
+
+            driver.Quit();
+        }
+    }
+}
