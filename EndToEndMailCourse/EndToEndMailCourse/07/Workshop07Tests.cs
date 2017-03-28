5fa9a36e97e9e98776018da23be2eb532160b9b1
Adrian Sroka <adrian.sroka@britishcouncil.pl>
2017-03-28 09:19:01 +0200

    Lesson 7 files

---
+++ EndToEndMailCourse/EndToEndMailCourse/07/Workshop07Tests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Linq;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+using System;
+
+namespace EndToEndMailCourse._07
+{
+    [TestFixture]
+    public class Workshop07Tests
+    {
+        private string testUrl = "https://suvroc.github.io/selenium-mail-course/07/workshop.html";
+
+        [Test]
+        public void ShouldSelectAnswers()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(testUrl);
+
+            var companySelectElement = driver.FindElement(By.Id("companySelect"));
+            var carSelectElement = driver.FindElement(By.Id("carSelect"));
+
+            #region TEST CODE
+
+            #endregion
+
+            Assert.AreEqual(driver.FindElements(By.ClassName("list-group-item")).Count(), 6);
+
+
+            driver.Quit();
+        }
+    }
+}
